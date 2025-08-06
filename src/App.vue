4136fa9066e1a8ef25f1db144451e80a7c94a06e
shiqian <shiqian@gmail.com>
2025-08-06 17:45:39 +0800

    Add Tencent Cloud UI Kit dependencies and update App.vue structure
    - Added @tencentcloud/chat-uikit-vue dependency to package.json and package-lock.json.
    - Updated App.vue to integrate TUIKit and TUICallKit components, replacing the previous template structure.
    - Modified tsconfig.app.json to include verbatimModuleSyntax option for improved TypeScript handling.

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,16 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div id="app">
+    <TUIKit :SDKAppID="0" userID="xxx" userSig="xxx" />
+    <TUICallKit class="callkit-container" :allowedMinimized="true" :allowedFullScreen="false" />
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
+<script lang="ts" setup>
+import { TUIKit } from './TUIKit';
+import { TUICallKit } from '@tencentcloud/call-uikit-vue';
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+const SDKAppID = 20026251;
+const userID = '20026251';
+const userSig = 'eJyrVgrxCdYrSy1SslIy0jNQ0gHzM1NS80oy0zIhwgYGRmZGpoZQueKU7MSCgswUJSuYBEQ8taIgsyhVycrQ1NQUKGMAES3JzAWJmZuamJgbGxibQs3ITAca7G-m5BQUVpqR7BxpYuAfUhrmVFVpnhPhVBmjn1qR4ZVj5ORn7hZZGhgYlRdqq1QLAOIGMMA_';
+</script>
+<style lang="scss">
 </style>

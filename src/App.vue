39d851fa067e2ac6a73e604dcdf8ac5e375c5064
shiqian <shiqian@gmail.com>
2025-08-06 17:07:44 +0800

    Refactor Vue 3 + Vite project structure to TypeScript, update dependencies, and enhance configuration. Replace JavaScript files with TypeScript, streamline .gitignore, and revise README for clarity. Introduce new tsconfig files for improved TypeScript support.

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,30 @@
-<template>
-  <div id="app">
-    <router-view />
-  </div>
-</template>
-
-<script setup>
-// 组件逻辑
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
 </script>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-  margin: 0;
-  padding: 0;
-}
+<template>
+  <div>
+    <a href="https://vite.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+</template>
 
-* {
-  box-sizing: border-box;
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
 }
-
-body {
-  margin: 0;
-  padding: 0;
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
 }
-</style> 
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
